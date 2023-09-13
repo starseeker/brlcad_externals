c3c6827b0398bb66172fd76f02098c64184e1aff
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-09-13 13:44:20 -0400

    First stage of getting openimageio to build.  Not done yet - it's not properly using bundled zlib and png, and it's pulling in system libs for things like webp - looking like we may have to patch to turn off support for such things manually...

---
+++ openimageio/openimageio/testsuite/python-colorconfig/src/test_colorconfig.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Copyright Contributors to the OpenImageIO project.
+# SPDX-License-Identifier: Apache-2.0
+# https://github.com/OpenImageIO/oiio
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import OpenImageIO as oiio
+
+
+try:
+    config = oiio.ColorConfig()
+    print ("getNumColorSpaces =", config.getNumColorSpaces())
+    print ("getColorSpaceNames =", config.getColorSpaceNames())
+
+    print ("getNumLooks =", config.getNumLooks())
+    print ("getLookNames =", config.getLookNames())
+
+    print ("getNumDisplays =", config.getNumDisplays())
+    print ("getDisplayNames =", config.getDisplayNames())
+    print ("getDefaultDisplayName =", config.getDefaultDisplayName())
+
+    print ("getNumViews =", config.getNumViews())
+    print ("getViewNames =", config.getViewNames())
+    print ("getDefaultViewName =", config.getDefaultViewName())
+
+    print ("getNumRoles =", config.getNumRoles())
+    print ("getRoles =", config.getRoles())
+
+    print ("")
+
+    print ("Done.")
+except Exception as detail:
+    print ("Unknown exception:", detail)
+
